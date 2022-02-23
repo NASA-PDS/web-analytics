26532b6e4b138cbbeed84d1ffca9ef5f9449d2da
Kai Pak <kaipak@jpl.nasa.gov>
2022-02-22 16:26:08 -0800

    Notebook reshuffling. Code for data processing seems to work. Testing with export to Feather.

--- src/clf_parser.py
+++ src/clf_parser.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+import time
 from apachelogs import LogParser
 
 COLUMN_NAMES = ['ip', 'identd', 'userid', 'date', 'time', 'timezone',
@@ -15,15 +17,25 @@ class CLFParse(object):
 
     def parse_files(self):
         """"""
+        print(f"Logfiles list contains {len(self.logfiles)} files.")
+        tick = time.time()
+        counter = 1
         for file in self.logfiles:
+            if counter % 10 == 0:
+                tock = time.time()
+                curr_time = np.round((tock - tick) / 60, 2)
+                print(f"{counter} files have been processed. {curr_time} minutes elapsed")
             log = open(file)
             lines = log.readlines()
             log_parsed = list(map(self._parse_line, lines))
             self.df_logs = pd.concat([self.df_logs, pd.DataFrame(log_parsed, columns=COLUMN_NAMES)])
+            counter += 1
+        elapsed = np.round((time.time() - tick) / 60, 2)
+        print(f"{elapsed}m to process log files.")
 
     def _parse_line(self, line):
         """Process line from logfile"""
-        parsed = parser.parse(line)
+        parsed = PARSER.parse(line)
         datetime = parsed.request_time
         parsed_line = [parsed.remote_host,
                        parsed.remote_logname,
