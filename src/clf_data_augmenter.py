9f8ab92266e2c23f12f1c8200eac056fe880b160
Kai Pak <kaipak@jpl.nasa.gov>
2022-03-06 17:27:06 -0800

    - Correct issue with computing GB/day on new notebook.

--- src/clf_data_augmenter.py
+++ src/clf_data_augmenter.py
@@ -9,7 +9,17 @@ class CLFDataAugmenter(object):
 
     def __init__(self, data: pd.DataFrame):
         self.df = data
+        self.augmented = False
+
+
+    def augment_data(self):
+        """Data mutations and augmentations """
+
+        self.df = self.df.astype({'status': int, 'size': int, 'hour': int}, errors='ignore')
         self.df['req_type'] = self.df['request'].str.split().str.get(0)
+        self.df['date'] = pd.to_datetime(self.df['date'])
+        self.df['month_year'] = pd.to_datetime(self.df['month_year'])
+
         # Assume if I can't categorize the tool, it's just basic content.
         self.df['tool'] = "content"
         self.df.loc[(~self.df['log_name'].str.contains("tool", na=False)) &
@@ -22,6 +32,8 @@ class CLFDataAugmenter(object):
                    (self.df['request'].str.contains('\/viewmaster')), 'tool'] = "viewmaster"
         self.df.loc[self.df['log_name'] == 'tools2', 'tool'] = 'opus'
         self.df.loc[self.df['log_name'] == 'tools', 'tool'] = 'opus'
+        self.augmented = True
 
     def get_df(self):
+        print(f"Data augmented? {self.augmented}")
         return self.df
