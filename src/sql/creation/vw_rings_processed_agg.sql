3c705bae866e1f3c83ae4b864032fcda103c5e96
Kai Pak <kaipak@jpl.nasa.gov>
2022-04-05 16:33:48 -0700

    Finalize nodes' processed table with detailed aggregated data including ip, client_request, and user_agent.

--- src/sql/creation/vw_rings_processed_agg.sql
+++ src/sql/creation/vw_rings_processed_agg.sql
@@ -1,18 +1,19 @@
-CREATE VIEW
-  vw_rings_processed AS
+CREATE TABLE prd_tbl_rings_agg AS
 SELECT
     DATE(DATE_PARSE(SPLIT(datetime, ' ')[1], '[%d/%M/%Y:%H:%i:%s')) as date,
     node,
-    --client_ip,
-    SPLIT(client_request, ' ')[1] as req_type,
-    --client_request,
+    CASE WHEN SPLIT(client_request, ' ')[1] IN ('GET', 'HEAD', 'POST', 'PUT',
+                                                'DELETE', 'CONNECT', 'OPTIONS',
+                                                'TRACE', 'PATCH')
+         THEN SPLIT(client_request, ' ')[1]
+         ELSE 'UNK'
+    END as http_method,
     status,
-    --user_agent,
     CASE regexp_like(user_agent, 'ahrefs|Barkrowler|Baiduspider|bingbot|CCBot|Cliqzbot|cs.daum\.net|DataForSeoBot|DomainCrawler|DuckDuckGo|Exabot|Googlebot|linkdexbot|magpi|-crawler|MauiBot|MJ12bot|MTRobot|msnbot|OpenLinkProfiler\.org|opensiteexplorer|pingdom|rogerbot|SemrushBot|SeznamBot|sogou\.com|tt-rss|Wotbox|YandexBot|YandexImages|ysearch\/slurp|BLEXBot|Flamingo_SearchEngine|okhttp|scalaj-http|UptimeRobot|YisouSpider|proximic\.com\/info\/spider/i')
         WHEN TRUE THEN 'robot'
         ELSE 'human'
     END as user_type,
-    CASE 
+    CASE
     WHEN regexp_like(client_request, 'tools') AND regexp_like(client_request, 'viewer') THEN 'viewer'
     WHEN regexp_like(client_request, 'tools') AND regexp_like(client_request, 'tracker') THEN 'tracker'
     WHEN regexp_like(client_request, 'tools') AND regexp_like(client_request, 'ephem') THEN 'ephem'
@@ -20,13 +21,15 @@ SELECT
     WHEN regexp_like(client_request, 'tools') AND regexp_like(client_request, 'viewmaster') THEN 'viewmaster'
     ELSE 'archive'
     END AS request_class,
+    client_request,
+    user_agent,
+    client_ip,
     sum(cast(replace(size, '-', '0') as bigint)) as bytes,
     count(client_ip) as transactions
-FROM "pds_analytics"."tbl_pds_all_logs_raw"
+FROM "pds_analytics"."prd_tbl_all_det"
 WHERE
     1 = 1
     AND CARDINALITY(SPLIT(size, ' ')) = 1
     AND node = 'rings'
-GROUP BY 1, 2, 3, 4, 5, 6
-ORDER BY
-    date(DATE_PARSE(SPLIT(datetime, ' ')[1], '[%d/%M/%Y:%H:%i:%s'))
+GROUP BY 1, 2, 3, 4, 5, 6, 7, 8, 9
+ORDER BY 1
--- src/sql/scratch.sql
+++ src/sql/scratch.sql
@@ -10,6 +10,25 @@ group by 1;
 
 select distinct date from  pds_analytics.prd_vw_img_agg;
 
-select http_method, count(*)
+select http_method, sum()
 from pds_analytics.tbl_test
 group by 1;
+
+select request_class, http_method, sum(bytes) / pow(2, 30), sum(transactions) from pds_analytics.tbl_test
+group by 1, 2
+order by 1, 2;
+
+
+select * from pds_analytics.prd_tbl_rings_agg
+where transactions > 1;
+
+select count(*) from pds_analytics.prd_tbl_rings_agg
+
+select * from pds_analytics.prd_tbl_rings_agg
+where request_class <> 'archive'
+limit 1000;
+
+
+select request_class, http_method, status, sum(bytes) / pow(2, 30) as gigabytes, count(*) as requests
+from pds_analytics.prd_tbl_rings_agg
+group by 1, 2, 3
