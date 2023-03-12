1a4b2c2edd077ca68770f43322a089863791e567
Kai Y Pak <kaipak@pds-rpt.jpl.nasa.gov>
2023-03-11 21:16:59 -0800

    Filtered aws set for EN and IMG.

--- scripts/s3_log_sync.sh
+++ scripts/s3_log_sync.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-# This script should really not be run as is. Just collection of commands run to sync weblogs with S3. Set to dry run so# it outputs files that would be sync'd but doesn't actually copy data.
-# 
 
-# 2021 and 2022
-# Rings
-aws --profile saml-pub s3 sync ./rings s3://pds-web-analytics/rings --exclude "*" --include "*2021*" --include "*2022*" --exclude "*.gz"
+BASE_LOGDIR="/report_service/logs/final"
 
-# Atmos
-aws --profile saml-pub s3 sync atm/ s3://pds-web-analytics/atm --exclude '*' --include '*202*' --include '*201*' --exclude '*ncftpd*' --dryrun
+# Engineering
+aws --profile saml-pub s3 sync ${BASE_LOGDIR}/en s3://pds-web-analytics-cloud/logs/en --exclude "*" --include "en-http-pdscloud*/*.txt.gz" 
 
 # Imaging
-aws --profile saml-pub s3 sync img/ s3://pds-web-analytics/img --exclude '*' --include '*2021*.log' --include '*2022*.log' --include '*log.2021*' --include '*log.2022*' --dryrun
+aws --profile saml-pub s3 sync ${BASE_LOGDIR}/img s3://pds-web-analytics-cloud/logs/img --exclude "*" --include "img-pdsimg*"
 
