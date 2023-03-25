5bf8852952f14b6cd6d5a26e0ae3002a9087471c
Kai Y Pak <kaipak@pds-rpt.jpl.nasa.gov>
2023-03-25 15:24:58 -0700

    Update sync command to use CLI version as that has much better behavior than hacked boto3 command.

--- scripts/s3_log_sync.py
+++ scripts/s3_log_sync.py
@@ -3,109 +3,115 @@
 
 
 """
-import boto3
 from box import Box
 import yaml
-import re
-import os
-import time
-from multiprocessing import Process
-
-import os
-import sys
-import boto3
-from boto3.s3.transfer import TransferConfig
-from tqdm import tqdm
-import boto3
-import os
-import time
-from multiprocessing import Process
-from tqdm import tqdm
 import io
+import os
+import re
+import sys
+from tqdm import tqdm
+import time
+import math
+import subprocess
+from multiprocessing import Pool, cpu_count
 
 
-class S3Sync:
-    def __init__(self, local_dirs, bucket_name, s3_prefix, profile_name):
-        self.local_dirs = local_dirs
+class S3Sync2:
+    def __init__(self,
+                 src_paths,
+                 src_logdir,
+                 bucket_name,
+                 s3_subdir,
+                 profile_name=None,
+                 delete=False,
+                 workers=None):
+        self.src_paths = src_paths
+        self.src_logdir = src_logdir
         self.bucket_name = bucket_name
-        self.s3_prefix = s3_prefix
+        self.s3_subdir = s3_subdir
         self.profile_name = profile_name
-        self.session = boto3.Session(profile_name=self.profile_name)
-        self.s3 = self.session.resource('s3')
+        self.delete = delete
+        self.workers = workers if workers else cpu_count()
+        self.s3_sync_cmd = ["aws", "s3", "sync"]
+        if self.profile_name:
+            self.s3_sync_cmd += ["--profile", self.profile_name]
+        self.s3_sync_cmd += [
+            "--exclude",
+            ".DS_Store"  # exclude macOS metadata files
+        ]
 
-    def sync_local_dir(self, local_dir):
-        # Get list of files in local directory
-        local_files = []
-        for root, dirs, files in os.walk(local_dir):
-            for file in files:
-                local_files.append(os.path.join(root, file))
+    def run(self):
+        pool = Pool(processes=self.workers)
+        pool.map(self.sync_directory, self.src_paths)
+        pool.close()
+        pool.join()
 
-        # Get last sync time from S3
-        s3_key = os.path.join(self.s3_prefix, 'last_sync_time')
-        try:
-            last_sync_time = float(self.s3.Object(self.bucket_name, s3_key).get()['Body'].read().decode())
-        except:
-            last_sync_time = 0
+    def sync_directory(self, src_path):
+        # Use subprocess to call the AWS CLI command and display progress
+        s3_path = os.path.join(self.s3_subdir, os.path.relpath(src_path, self.src_logdir))
+        cmd = self.s3_sync_cmd + [src_path, f"s3://{self.bucket_name}/{s3_path}"]
+        proc = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1
+        )
+        # Use os.walk to get total size of directory (for progress bar)
+        total_size = sum(
+            os.path.getsize(os.path.join(dirpath, filename))
+            for dirpath, _, filenames in os.walk(src_path)
+            for filename in filenames
+        )
 
-        # Calculate total size of files that need to be synced
-        total_size = 0
-        files_to_sync = []
-        for local_file in local_files:
-            if os.path.getmtime(local_file) > last_sync_time:
-                total_size += os.path.getsize(local_file)
-                files_to_sync.append(local_file)
+        # Display progress bar
+        sent = 0
+        for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+            print(line.rstrip())
+            if "sent" in line:
+                if line.split()[2] is not None:
+                    sent += int(line.split()[2])
+                    progress = sent / total_size
+                    print(f"{src_path} - "
+                          f"{self.convert_size(sent)} / {self.convert_size(total_size)} - "
+                          f"{progress:.0%} - "
+                          f"{self.get_throughput(sent)}")
+                else:
+                    continue
+        for line in io.TextIOWrapper(proc.stderr, encoding="utf-8"):
+            print(line.rstrip())
 
-        # Sync new and modified files to S3
-        transmitted_size = 0
-        processed_files = 0
-        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc=f"Uploading {local_dir}")
-        start_time = time.time()
-        for local_file in files_to_sync:
-            s3_key = os.path.join(self.s3_prefix, os.path.relpath(local_file, local_dir))
-            with open(local_file, 'rb') as f:
-                file_contents = io.BytesIO(f.read())
-            size = os.path.getsize(local_file)
-            transmitted_size += size
-            progress_bar.update(size)
-            progress_bar.set_postfix(files_transmitted=f"{processed_files+1}/{len(files_to_sync)}", current_file=local_file)
-            self.s3.Object(self.bucket_name, s3_key).upload_fileobj(file_contents, Callback=lambda sent: progress_bar.update(sent - progress_bar.n))
+        # Wait for the subprocess to finish
+        proc.wait()
+        if proc.returncode != 0:
+            raise Exception(f"S3 sync failed with return code {proc.returncode}")
 
-            processed_files += 1
+    @staticmethod
+    def convert_size(size_bytes):
+        # Convert bytes to a human-readable format
+        if size_bytes == 0:
+            return "0B"
+        size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+        i = int(math.floor(math.log(size_bytes, 1024)))
+        p = math.pow(1024, i)
+        s = round(size_bytes / p, 2)
+        return f"{s}{size_name[i]}"
 
-        end_time = time.time()
+    @staticmethod
+    def get_throughput(sent):
+        # Calculate and return the current throughput in MB/s
+        elapsed_time = time.monotonic() - start_time
+        mb_sent = sent / (1024 * 1024)
+        return f"{mb_sent / elapsed_time:.2f} MB/s"
 
-        # Calculate throughput and display it in the progress bar
-        duration = end_time - start_time
-        throughput = total_size / (1024 ** 3) / duration
-        progress_bar.set_description(f"Uploading {local_dir} ({round(throughput, 2)} GB/s)")
-
-        # Update last sync time in S3
-        last_sync_time = time.time()
-        s3_key = os.path.join(self.s3_prefix, 'last_sync_time')
-        self.s3.Object(self.bucket_name, s3_key).put(Body=str(last_sync_time))
-
-    def sync(self):
-        processes = []
-        for local_dir in self.local_dirs:
-            p = Process(target=self.sync_local_dir, args=(local_dir,))
-            p.start()
-            processes.append(p)
-
-        for p in processes:
-            p.join()
 
 if __name__ == '__main__':
     # Parse basic config file and set up AWS Session
     with open('../config/def.yaml', 'r') as file:
         config = yaml.safe_load(file)
     config = Box(config, box_dots=True)
-    local_dirs = [config.log_directory + "/" + node for node in config.node_dirs]
-    s3_sync = S3Sync(profile_name=config.profile_name,
-                     local_dirs=local_dirs,
-                     bucket_name=config.s3_bucket, s3_prefix=config.s3_logdir)
-    # s3_sync.sync()
-    print(type(config.subdirs))
-    print([config.log_directory + "/" + dir + "/" + subdir
-           for dir in config.subdirs.keys()
-           for subdir in config.subdirs[dir]])
-
+    local_dirs = [config.log_directory + "/" + dir + "/" + subdir
+                  for dir in config.subdirs.keys()
+                  for subdir in config.subdirs[dir]]
+    s3_sync = S3Sync2(src_paths=local_dirs,
+                      src_logdir=config.log_directory,
+                      bucket_name=config.s3_bucket,
+                      s3_subdir=config.s3_logdir,
+                      profile_name=config.profile_name)
+    s3_sync.run()
