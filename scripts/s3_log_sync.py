2c64445daa71a3f07422ed2aa35a6085aa302f95
Kai Y Pak <kaipak@pds-rpt.jpl.nasa.gov>
2023-04-10 18:23:51 -0700

    Fix progress report as script syncs directories.

--- scripts/s3_log_sync.py
+++ scripts/s3_log_sync.py
@@ -13,19 +13,10 @@ from tqdm import tqdm
 import time
 import math
 import subprocess
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, Value
 
-class S3Sync3:
-    start_time = time.monotonic()
-
-    def __init__(self,
-                 src_paths,
-                 src_logdir,
-                 bucket_name,
-                 s3_subdir,
-                 profile_name=None,
-                 delete=False,
-                 workers=None):
+class S3Sync2:
+    def __init__(self, src_paths, src_logdir, bucket_name, s3_subdir, profile_name=None, delete=False, workers=None):
         self.src_paths = src_paths
         self.src_logdir = src_logdir
         self.bucket_name = bucket_name
@@ -36,67 +27,72 @@ class S3Sync3:
         self.s3_sync_cmd = ["aws", "s3", "sync"]
         if self.profile_name:
             self.s3_sync_cmd += ["--profile", self.profile_name]
-        self.s3_sync_cmd += [
-            "--exclude",
-            ".DS_Store"  # exclude macOS metadata files
-        ]
-        self.total_size = 0
-        self.sent = 0
+        self.s3_sync_cmd += ["--exclude", ".DS_Store"]
 
     def run(self):
-        # Use os.walk to get total size of directories (for progress bar)
         for src_path in self.src_paths:
-            self.total_size += sum(
-                os.path.getsize(os.path.join(dirpath, filename))
-                for dirpath, _, filenames in os.walk(src_path)
-                for filename in filenames
-            )
-
-        pool = Pool(processes=self.workers)
-        pool.map(self.sync_directory, self.src_paths)
-        pool.close()
-        pool.join()
+            self.sync_directory(src_path)
 
     def sync_directory(self, src_path):
         # Use subprocess to call the AWS CLI command and display progress
+        start_time = time.monotonic()
         s3_path = os.path.join(self.s3_subdir, os.path.relpath(src_path, self.src_logdir))
         cmd = self.s3_sync_cmd + [src_path, f"s3://{self.bucket_name}/{s3_path}"]
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        while True:
-            #for line in proc.stdout:
-            for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+        # Use subprocess to call the AWS CLI command and display progress
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1,
+                              universal_newlines=True) as proc:
+            for line in proc.stdout:
                 if "Completed" in line:
-                    self.sent += int(float(line.split()[1]))
-                    progress = self.sent / self.total_size
-                    print(f"Overall Progress: {progress:.0%} - {self.convert_size(self.sent)} / "
-                          f"{self.convert_size(self.total_size)} - "
-                          f"{self.get_throughput(self.sent)}")
-            output = proc.stderr.readline().decode()
-            if output == "" and proc.poll() is not None:
-                break
-        # Wait for the subprocess to finish
-        proc.wait()
+                    try:
+                        line_components = line.split()
+                        sent = int((line_components[1]))
+                        sent_incre = line_components[2].split('/')[0]
+                        total_size = int(float(line_components[2].split('/')[1].replace("~", "")))
+                        total_size_incre =  line_components[3]
+                        sent = self.get_bytes(sent, sent_incre)
+                        total_size = self.get_bytes(total_size, total_size_incre)
+                        progress = sent / total_size
+                        print(f"{src_path} - "
+                              f"{self.convert_size(sent)} / {self.convert_size(total_size)} - "
+                              f"{progress:.0%} - "
+                              f"{self.get_throughput(sent, start_time)}", end="\r")
+                    except Exception as e:
+                        print(f"Error parsing aws cli output: {e}", end="\r")
+            print(f"Completed syncing {src_path}")
+
+        if self.delete:
+            print(f"Deleting local files from {src_path}")
+            shutil.rmtree(src_path)
 
     @staticmethod
-    def convert_size(size_bytes):
-        """
-        Convert bytes to a human-readable format
-        """
-        if size_bytes == 0:
+    def convert_size(size):
+        # Convert bytes to a human-readable format
+        if size == 0:
             return "0B"
         size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-        i = int(math.floor(math.log(size_bytes, 1024)))
+        i = int(math.floor(math.log(size, 1024)))
         p = math.pow(1024, i)
-        s = round(size_bytes / p, 2)
+        s = round(size / p, 2)
         return f"{s}{size_name[i]}"
 
-    #@staticmethod
-    def get_throughput(self, sent):
+    @staticmethod
+    def get_bytes(size, size_incre):
+        if size_incre == "KiB":
+            size *= 2**10
+        if size_incre == "MiB":
+            size *= 2**20
+        if size_incre == "GiB":
+            size *= 2**30
+        if size_incre == "TiB":
+            size *= 2**40
+        return size
+
+    @staticmethod
+    def get_throughput(sent, start_time):
         # Calculate and return the current throughput in MB/s
-        elapsed_time = time.monotonic() - self.start_time
+        elapsed_time = time.monotonic() - start_time
         mb_sent = sent / (1024 * 1024)
         return f"{mb_sent / elapsed_time:.2f} MB/s"
-
 
 
 if __name__ == '__main__':
@@ -107,7 +103,7 @@ if __name__ == '__main__':
     local_dirs = [config.log_directory + "/" + dir + "/" + subdir
                   for dir in config.subdirs.keys()
                   for subdir in config.subdirs[dir]]
-    s3_sync = S3Sync3(src_paths=local_dirs,
+    s3_sync = S3Sync2(src_paths=local_dirs,
                       src_logdir=config.log_directory,
                       bucket_name=config.s3_bucket,
                       s3_subdir=config.s3_logdir,
