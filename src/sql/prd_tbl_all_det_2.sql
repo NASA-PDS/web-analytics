90cbc6692ac3989abc016907a80abcc9505952c1
Kai Y Pak <kaipak@pds-rpt.jpl.nasa.gov>
2024-03-13 15:03:33 -0700

    Minor updates to SQL queries.

--- src/sql/prd_tbl_all_det_2.sql
+++ src/sql/prd_tbl_all_det_2.sql
@@ -5,11 +5,11 @@
 */
 
 ALTER TABLE pds_analytics.prd_tbl_all_det ADD
-PARTITION (node='atm') location 's3://pds-web-analytics/atm'
-PARTITION (node='geo') location 's3://pds-web-analytics/geo'
-PARTITION (node='img') location 's3://pds-web-analytics/img'
-PARTITION (node='naif') location 's3://pds-web-analytics/naif'
-PARTITION (node='ppi') location 's3://pds-web-analytics/ppi'
-PARTITION (node='rings') location 's3://pds-web-analytics/rings'
-PARTITION (node='sbn') location 's3://pds-web-analytics/sbn'
-PARTITION (node='en') location 's3://pds-web-analytics/en';
+PARTITION (node='atm') location 's3://pds-web-analytics/logs/atm'
+PARTITION (node='geo') location 's3://pds-web-analytics/logs/geo'
+PARTITION (node='img') location 's3://pds-web-analytics/logs/img'
+PARTITION (node='naif') location 's3://pds-web-analytics/logs/naif'
+PARTITION (node='ppi') location 's3://pds-web-analytics/logs/ppi'
+PARTITION (node='rings') location 's3://pds-web-analytics/logs/rings'
+PARTITION (node='sbn') location 's3://pds-web-analytics/logs/sbn'
+PARTITION (node='en') location 's3://pds-web-analytics/logs/en';
--- src/sql/prd_tbl_en_search.sql
+++ src/sql/prd_tbl_en_search.sql
@@ -1,4 +1,4 @@
-CREATE TABLE prd_tbl_en AS
+CREATE TABLE prd_tbl_en_search AS
 SELECT DATE(DATE_PARSE(SPLIT(datetime, ' ')[1], '[%d/%M/%Y:%H:%i:%s')) as date,
 lower(regexp_replace(regexp_extract(client_request, 'GET \/datasearch\/keyword-search\/search.jsp\?q\=([\w\+\-\%]+)', 1), '(\+|(\%20))', ' ')) as query_text,
 client_request, client_ip, datetime, status,
@@ -8,6 +8,7 @@ from pds_analytics.prd_tbl_all_det
 where 1=1
 and node = 'en'
 and client_request like '%GET /datasearch/keyword--search/search.jsp?q=%' ESCAPE '-'
+and SPLIT(datetime, ' ')[1] <> '-'
 
 
 
