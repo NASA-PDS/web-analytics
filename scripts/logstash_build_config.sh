ba75da5ea09bb5372eb900cbee7a7a50fe196a47
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-06-18 13:42:41 -0700

    Update configs and build script

--- scripts/logstash_build_config.sh
+++ scripts/logstash_build_config.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Check if LOGSTASH_CONF_HOME is set
-if [ -z "$LOGSTASH_CONF_HOME" ]; then
-  echo "Error: LOGSTASH_CONF_HOME environment variable is not set"
-  echo "Please set LOGSTASH_CONF_HOME to the path of your Logstash configuration directory"
+# Check if LS_SETTINGS_DIR is set
+if [ -z "$LS_SETTINGS_DIR" ]; then
+  echo "Error: LS_SETTINGS_DIR environment variable is not set"
+  echo "Please set LS_SETTINGS_DIR to the path of your Logstash configuration directory"
   exit 1
 fi
 
-INPUT_DIR="$LOGSTASH_CONF_HOME/inputs"
-SHARED_FILTER="$LOGSTASH_CONF_HOME/shared/pds-filter.conf"
-SHARED_OUTPUT="$LOGSTASH_CONF_HOME/shared/pds-output-opensearch.conf"
-PIPELINE_DIR="$LOGSTASH_CONF_HOME/pipelines"
+INPUT_DIR="$LS_SETTINGS_DIR/inputs"
+SHARED_FILTER="$LS_SETTINGS_DIR/shared/pds-filter.conf"
+SHARED_OUTPUT="$LS_SETTINGS_DIR/shared/pds-output-opensearch.conf"
+PIPELINE_DIR="$LS_SETTINGS_DIR/pipelines"
 
-for input_file in $LOGSTASH_CONF_HOME/inputs/*.conf; do
+mkdir -p $LS_SETTINGS_DIR/pipelines
+
+for input_file in $LS_SETTINGS_DIR/inputs/*.conf; do
   name=$(basename "$input_file" .conf)
-  cat "$input_file" $LOGSTASH_CONF_HOME/shared/pds-filter.conf $LOGSTASH_CONF_HOME/shared/pds-output-opensearch.conf \
-    > "$LOGSTASH_CONF_HOME/pipelines/pipeline-${name}.conf"
+  cat "$input_file" $SHARED_FILTER $SHARED_OUTPUT \
+    > "$PIPELINE_DIR/pipeline-${name}.conf"
 done
