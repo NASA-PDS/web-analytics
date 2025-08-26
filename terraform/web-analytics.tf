92f1aac0e7218ba4fe768498dd4ee179b967199f
Sagar Joshi <sagar.joshi@jpl.nasa.gov>
2025-08-26 12:26:26 -0700

    HTTPS-only access sonarqube warn

--- terraform/web-analytics.tf
+++ terraform/web-analytics.tf
@@ -5,6 +5,24 @@ resource "aws_s3_bucket" "web_analytics" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "pab" {
+  bucket                  = aws_s3_bucket.web_analytics.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.web_analytics.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.web_analytics.id
   versioning_configuration {
