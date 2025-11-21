a9e24eccdf755c8cb2e59a92b5e0ce0fa01b5e99
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-11-21 09:35:24 -0800

    Replace pkg_resources with standard library modules
    Replace deprecated pkg_resources with standard library alternatives to
    fix ModuleNotFoundError in tox test environments.
    
    Changes:
    - Replace pkg_resources.resource_string() with importlib.resources.files()
      in pds/web_analytics/__init__.py for reading VERSION.txt
    - Replace pkg_resources.declare_namespace() with pkgutil.extend_path()
      in pds/__init__.py for namespace package declaration
    
    Both importlib.resources and pkgutil are part of the Python standard
    library (available in Python 3.9+ and 3.3+ respectively), eliminating
    the dependency on setuptools/pkg_resources at runtime.
    
    This fixes the ImportError when running tox tests:
      ModuleNotFoundError: No module named 'pkg_resources'
    
    Fixes tox test failures for Python 3.12 and 3.13 compatibility.

--- src/pds/__init__.py
+++ src/pds/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 """PDS Namespace."""
 
-__import__("pkg_resources").declare_namespace(__name__)
+# Use pkgutil for namespace packages (PEP 420 compatible, no external dependencies)
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
