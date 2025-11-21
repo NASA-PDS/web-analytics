28a759412511557795062b52894c8582ed57e1aa
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-11-21 10:09:06 -0800

    Fix linter issues

--- src/pds/__init__.py
+++ src/pds/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 """PDS Namespace."""
-
 # Use pkgutil for namespace packages (PEP 420 compatible, no external dependencies)
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
